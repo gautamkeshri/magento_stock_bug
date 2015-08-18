3215128f130579e7f3c2348dea8b0e6c1b8c5c87
gautamkeshri <gautamkeshri@gmail.com>
2015-08-18 15:40:33 +0530

    v0.1

--- checkstock.php
+++ checkstock.php
@@ -1,6 +1,5 @@
 <?php
-defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
-
+require_once('../app/Mage.php');
 ini_set("display_errors", 1);
 set_time_limit(0);
 umask(0);
@@ -8,10 +7,8 @@ Mage::app()->setCurrentStore(Mage_Core_Model_App::ADMIN_STORE_ID);
 
 class Checkstatus
 {
-	public function setProdcollection()	{
-		$this->collectionConfigurable =  Mage::getResourceModel('catalog/product_collection')->addAttributeToFilter('type_id', array('eq' => 'configurable'));
-	}	
-	public function setBackInStock()
+	
+	/*public function setBackInStock()
 	{
 	    $collection = Mage::getResourceModel('cataloginventory/stock_item_collection');
 	    $outQty = Mage::getStoreConfig('cataloginventory/item/options_min_qty');
@@ -22,7 +19,7 @@ class Checkstatus
 	        $item->setData('is_in_stock', 1);
 	    }
 	    $collection->save();
-	}
+	}*/
 	public function setBackOutStock()
 	{
 	    $collection = Mage::getResourceModel('cataloginventory/stock_item_collection');
@@ -30,11 +27,24 @@ class Checkstatus
 	    $collection->addFieldToFilter('qty', array('gt' => $outQty));
 	    $collection->addFieldToFilter('is_in_stock', 0);
 
-	    foreach($collection as $item) {
+	    /*foreach($collection as $item) {
 	        $item->setData('is_in_stock', 1);
 	    }
 	    $collection->save();
+	    */
+	    return $collection;
+	}
+	public function main()
+	{
+		$configCollection = $this->setBackOutStock();
+		//$configCollection = Mage::getResourceModel('catalog/product_collection')->addAttributeToFilter('type_id', array('eq' => 'configurable'));
+		foreach($configCollection as $confProd)
+		{
+			echo "Id: ".$confProd->getProductId()." - Qty: ".$confProd->getQty()."\n\r";
+		}
 	}
 }
 
+$obj = new Checkstatus();
+$obj->main();
 ?>
